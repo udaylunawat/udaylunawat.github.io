6589024f4034e6ac145a75e62cdad28007ac1d3e
Uday Lunawat <udaylunawat@gmail.com>
2018-09-14 11:39:42 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+particlesJS("particles-js", 
 {
   "particles": {
     "number": {
@@ -107,4 +108,36 @@
     }
   },
   "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+
+
+
+function button_action() {
+  var element = document.getElementById("content");
+  var button = document.getElementById("button_hideshow");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+    button.innerHTML = "Hide";
+  } else {
+    element.style.display = "none";
+    button.innerHTML = "Show";
+  }
 }
