d30e3a653a7e9408a9c009c0cfba0c5c5fdcb884
Uday Lunawat <udaylunawat@gmail.com>
2018-09-14 11:46:57 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,113 +1,57 @@
-particlesJS("particles-js", 
-{
-  "particles": {
-    "number": {
-      "value": 303,
-      "density": {
-        "enable": true,
-        "value_area": 2288.6365250605704
-      }
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 200, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
     },
-    "color": {
-      "value": "#ffffff"
+    opacity: {
+      value: 0.6392398569996766,
+      random: true,
+      anim: { enable: true, speed: 2, opacity_min: 0, sync: false }
     },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: true, speed: 4, size_min: 0.3, sync: false }
     },
-    "opacity": {
-      "value": 1,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 1,
-        "opacity_min": 0,
-        "sync": false
-      }
+    line_linked: {
+      enable: true,
+      distance: 140,
+      color: "#1758b6",
+      opacity: 0.28,
+      width: 0.8
     },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 4,
-        "size_min": 0.3,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 173.62070190114673,
-      "color": "#252fd2",
-      "opacity": 0.2762147530245516,
-      "width": 0.7891850086415761
-    },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 600
-      }
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 }
     }
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "bubble"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "bubble" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
     },
-    "modes": {
-      "grab": {
-        "distance": 97.38283627510657,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 400,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 400, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
     }
   },
-  "retina_detect": true
+  retina_detect: true
 });
 var count_particles, stats, update;
 stats = new Stats();
