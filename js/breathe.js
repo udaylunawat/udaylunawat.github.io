f61847b8edeb608a220c403120a5b440356e88cc
udaylunawat <udaylunawat@gmail.com>
2022-07-12 03:06:51 +0530

    breathe! animation

---
+++ js/breathe.js
@@ -0,0 +1,18 @@
+const chars = document.querySelectorAll(".char");
+const headerName = document.querySelector(".author");
+
+let timeline = gsap.timeline();
+
+headerName.addEventListener("mouseover", () => {
+    timeline.from(chars, {opacity: 1, scale: 1, duration: 1, ease: "back"})
+        .to(chars, {
+            "--font-weight": 900,
+            ease: "power",
+            duration: 0.5,
+            stagger: {
+                each: 0.1,
+                repeat: -1,
+                yoyo: true
+            }
+        });
+});
