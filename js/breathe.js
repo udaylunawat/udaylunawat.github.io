16353483f4d709b4b6e611530aa825881493427f
udaylunawat <udaylunawat@gmail.com>
2022-07-12 05:39:58 +0530

    refactor

--- js/breathe.js
+++ js/breathe.js
@@ -6,9 +6,9 @@ let timeline = gsap.timeline();
 headerName.addEventListener("mouseover", () => {
     timeline.from(chars, {opacity: 1, scale: 1, duration: 1, ease: "back"})
         .to(chars, {
-            "--font-weight": 1200,
+            "--font-weight": 800,
             ease: "power",
-            duration: 0.5,
+            duration: 2.0,
             stagger: {
                 each: 0.1,
                 repeat: -1,
